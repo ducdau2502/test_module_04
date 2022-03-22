4cb74f602c61e10076a39db4a1f6141e1e53a7b0
ducdau2502 <ducdau2502@gmail.com>
2022-03-22 08:14:11 +0700

    add init

---
+++ src/main/java/com/test_module_04/TestModule04Application.java
@@ -0,0 +1,13 @@
+package com.test_module_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestModule04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestModule04Application.class, args);
+    }
+
+}
